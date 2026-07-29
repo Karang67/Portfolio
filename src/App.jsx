cd0d4933ff029803cabbc63da19730afefa5cb41
Student karan <Studentkarangehlot5686@gmail.com>
2026-07-29 09:58:53 +0530

    Set profile sidebar position to fixed sticky in desktop view during page scroll

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,8 @@ function App() {
       {/* Main Container */}
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-24 lg:pt-28 pb-12 relative z-10">
         <div className="flex flex-col lg:flex-row gap-8 items-start">
-          {/* Profile Sidebar (Static layout) */}
-          <div className="w-full lg:w-[320px]">
+          {/* Sticky Profile Sidebar */}
+          <div className="w-full lg:w-[320px] lg:sticky lg:top-28">
             <Sidebar onOpenResume={() => setIsResumeModalOpen(true)} />
           </div>
 
