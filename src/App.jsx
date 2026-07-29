34541ce682fc7406e0c137fe04475bbbe6698279
Student karan <Studentkarangehlot5686@gmail.com>
2026-07-29 10:04:41 +0530

    Fix left profile sidebar in place on desktop while right main section scrolls smoothly

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,8 @@ function App() {
       {/* Main Container */}
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-24 lg:pt-28 pb-12 relative z-10">
         <div className="flex flex-col lg:flex-row gap-8 items-start">
-          {/* Sticky Profile Sidebar */}
-          <div className="w-full lg:w-[320px] lg:sticky lg:top-28">
+          {/* Fixed Desktop Profile Sidebar (Only right main content scrolls) */}
+          <div className="w-full lg:w-[320px] shrink-0 lg:sticky lg:top-24 lg:max-h-[calc(100vh-7rem)] lg:overflow-y-auto">
             <Sidebar onOpenResume={() => setIsResumeModalOpen(true)} />
           </div>
 
